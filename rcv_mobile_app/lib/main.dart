9f3854ab5e933d2263b5721c1527e5deda559cca
Daniela Obushcharova <67962127+DanielaObushcharova@users.noreply.github.com>
2022-09-16 15:05:55 +0300

    add camera page

--- rcv_mobile_app/lib/main.dart
+++ rcv_mobile_app/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:rcv_mobile_app/camera.dart';
 /* file: main.dart */
 
+List<Camera> cameras = [];
+
 void main() {
   runApp(const MyApp());
 }
@@ -26,8 +28,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  List<Camera> cameras = [];
-
   @override
   void initState() {
     super.initState();
@@ -39,13 +39,59 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Cameras")),
+      appBar: AppBar(
+          title: const Text("Cameras")
+      ),
       body: ListView.builder(
         itemCount: cameras.length,
         itemBuilder: (context, index) {
-          return ListTile(title: Text(cameras[index].name));
+          return ListTile(
+              title: Text(
+                  cameras[index].name
+              ),
+              onTap: () { // NEW from here .// ..
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return SecondPage(index: 1);
+                }));
+              });
         },
       ),
+    );
+  }
+}
+
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key, required this.index}) : super(key: key);
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Some camera'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Image.asset('assets/images/sample1.jpg',
+                height: 400,
+                width: 400,
+            ),
+            Text(
+                cameras[index].name
+            ),
+            ListTile(
+              //contentPadding: EdgeInsets.all(<some value here>),//change for side padding
+              title: Row(
+                children: <Widget>[
+                  Expanded(child: TextButton(onPressed: () {},child: Text("Вызвать полицию"))),
+                  Expanded(child: TextButton(onPressed: () {},child: Text("Открыть дверь"))),
+                ],
+              ),
+            ),
+          ], //<Widget>[]
+        ), //Column
+      ), //Center
     );
   }
 }
