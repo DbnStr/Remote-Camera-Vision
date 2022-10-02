1fe94fc75600fe3934d84b9a2f8c1a54d266021b
DBNSTR <alexpich2002@yandex.ru>
2022-10-02 13:18:49 +0300

    change type of id

--- rcv_mobile_app/lib/models/person_model.dart
+++ rcv_mobile_app/lib/models/person_model.dart
@@ -2,7 +2,7 @@ import 'package:rcv_mobile_app/models/coordinates_model.dart';
 
 class Person {
 
-  final int? id;
+  final String? id;
   final String? name;
   final Coordinates? faceCoordinates;
 
